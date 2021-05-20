7b57f137fccf783b2146595dbd903f57989eb7cb
Meher450 <meherraju2006@gmail.com>
2021-05-20 16:51:40 +0530

    test  box

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var test;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    test = new Box(100,100,100,100);
 }
 
 function draw(){
@@ -61,4 +63,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    test.display();
 }
